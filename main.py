639050f4c44db21c20c741d12536350fb72b47d7
Dalton Paterson <daltonpaterson@gmail.com>
2025-06-04 21:50:40 +0100

    Adjusted main menu and implemented roulette menu

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
-if st.button("Blackjack"):
+# -----
+# Main Menu Layout
+# -----
+
+left, middle, right = st.columns(3, vertical_alignment="center")
+
+middle.divider()
+roulette = middle.button("Roulette", use_container_width=True)
+blackjack = middle.button("Blackjack", use_container_width=True)
+middle.divider()
+
+# -----
+# Navigation Functionality
+# -----
+
+if roulette:
+    st.switch_page("pages/roulette_menu.py")
+if blackjack:
     st.switch_page("pages/blackjack.py")
