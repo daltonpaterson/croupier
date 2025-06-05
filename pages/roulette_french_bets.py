410fdbd1c59e3c5d69265662454ce829916f42e7
Dalton Paterson <daltonpaterson@gmail.com>
2025-06-05 14:57:44 +0100

    Implemented French bets, minor house keeping

--- pages/roulette_french_bets.py
+++ pages/roulette_french_bets.py
@@ -3,21 +3,20 @@ import random
 import uuid
 
 DIFFICULTY_RANGES = {
-    "Easy": list(range(5, 101, 1)),     # 5 to 100
-    "Medium": list(range(5, 301, 1)),   # 5 to 300
-    "Hard": list(range(5, 501, 1)),   # 5 to 500
+    "Easy": list(range(5, 55, 5)),     # 5 to 50
+    "Medium": list(range(50, 105, 5)),   # 50 to 100
+    "Hard": list(range(100, 205, 5)),   # 100 to 200
 }
 
 # -------------------
 # Difficulty selection
 # -------------------
-
 if "difficulty_chosen" not in st.session_state:
     st.session_state.difficulty_chosen = False
     st.session_state.difficulty = None
 
 if not st.session_state.difficulty_chosen:
-    st.title("Cash Conversions")
+    st.title("French Bets")
     difficulty = st.selectbox(
         "Select Difficulty:",
         options=["Easy", "Medium", "Hard"],
@@ -25,42 +24,51 @@ if not st.session_state.difficulty_chosen:
     )
 
     left, middle, right = st.columns(3, vertical_alignment="center")
-    launch_2 = middle.button("£2", use_container_width=True)
-    launch_5 = middle.button("£5", use_container_width=True)
-    launch_25 = middle.button("£25", use_container_width=True)
+    launch_tier = middle.button("Tier", use_container_width=True)
+    launch_orphelins = middle.button("Orphelins", use_container_width=True)
+    launch_voisin = middle.button("Voisin", use_container_width=True)
+    launch_mixed = middle.button("Mixed", use_container_width=True)
     return_to_menu = middle.button("Return to Roulette Menu", use_container_width=True)
 
-    if launch_2:
+    if launch_tier:
         st.session_state.difficulty = difficulty
         st.session_state.difficulty_chosen = True
-        st.session_state.conversion_multiplier = 2
+        st.session_state.conversion_multiplier = 6
+        st.session_state.mixed = False
         st.rerun()
 
-    if launch_5:
+    if launch_orphelins:
         st.session_state.difficulty = difficulty
         st.session_state.difficulty_chosen = True
         st.session_state.conversion_multiplier = 5
+        st.session_state.mixed = False
         st.rerun()
 
-    if launch_25:
+    if launch_voisin:
         st.session_state.difficulty = difficulty
         st.session_state.difficulty_chosen = True
-        st.session_state.conversion_multiplier = 25
+        st.session_state.conversion_multiplier = 9
+        st.session_state.mixed = False
+        st.rerun()
+
+    if launch_mixed:
+        st.session_state.difficulty = difficulty
+        st.session_state.difficulty_chosen = True
+        st.session_state.mixed = True
         st.rerun()
 
     if return_to_menu:
         st.session_state.clear()
         st.switch_page("pages/roulette_menu.py")
 else:
-    # --------------------
-    # Conversion Logic
-    # --------------------
+    # Question Pool - Default range set to Easy
+    question_pool = DIFFICULTY_RANGES.get(st.session_state.difficulty, DIFFICULTY_RANGES["Easy"])
 
     def reset_question():
-        current = st.session_state.question_number
-        available = [q for q in question_pool if q != current]
-        st.session_state.question_number = random.choice(available)
-        st.session_state.correct_answer = st.session_state.question_number * st.session_state.conversion_multiplier
+        question = generate_question()
+        st.session_state.question_text = question[0]
+        st.session_state.question_number = question[1]
+        st.session_state.correct_answer = question[2]
         st.session_state.correct = False
         st.session_state.show_result = False
         st.session_state.show_answer = False
@@ -71,29 +79,49 @@ else:
         st.session_state.show_answer = True
         st.session_state.show_result = True
 
-    # Question Pool - Default range set to Easy
-    question_pool = DIFFICULTY_RANGES.get(st.session_state.difficulty, DIFFICULTY_RANGES["Easy"])
+    def generate_question():
+        if st.session_state.mixed == True:
+            bets = [("Tier", 6), ("Orphelins", 5), ("Voisin", 9)]
+            bet1, bet2 = random.sample(bets, 2)
+            combined_pieces = bet1[1] + bet2[1]
+            amount = random.choice(question_pool)
+
+            
+            return f"{bet1[0]} and {bet2[0]} by {amount}", amount, combined_pieces * amount
+        else:
+            if st.session_state.conversion_multiplier == 6:
+                french_bet = "Tier"
+            elif st.session_state.conversion_multiplier == 5:
+                french_bet = "Orphelins"
+            elif st.session_state.conversion_multiplier == 9:
+                french_bet = "Voisin"
+
+            amount = random.choice(question_pool)
+            
+            #Returns a tuple in the following format - Question text, amount to multiply, answer
+            return f"{french_bet} by {amount}", {amount}, st.session_state.conversion_multiplier * amount
 
     # Initialize session state for game
     if "question_number" not in st.session_state:
-        st.session_state.question_number = random.choice(question_pool)
-        st.session_state.correct_answer = st.session_state.question_number * st.session_state.conversion_multiplier
+        question = generate_question()
+        st.session_state.question_text = question[0]
+        st.session_state.question_number = question[1]
+        st.session_state.correct_answer = question[2]
         st.session_state.show_result = False
         st.session_state.correct = False
         st.session_state.show_answer = False
         st.session_state.input_key = str(uuid.uuid4())
 
-    st.title(f"Cash Conversions at £{st.session_state.conversion_multiplier}")
-    st.subheader(f"{st.session_state.question_number} at {st.session_state.conversion_multiplier}")
+    st.title(f"French Bets")
+    st.subheader(f"{st.session_state.question_text}")
 
     with st.form("answer_form"):
         user_input = st.text_input("What is the correct answer?", key=st.session_state.input_key)
         submitted = st.form_submit_button("Check", type = "primary")
-
         if submitted:
             try:
-                user_value = float(user_input)
-                correct_value = st.session_state.question_number * st.session_state.conversion_multiplier
+                user_value = int(user_input)
+                correct_value = st.session_state.correct_answer
                 is_correct = abs(user_value - correct_value) < 0.01
 
                 if st.session_state.correct and is_correct:
@@ -113,7 +141,7 @@ else:
         else:
             st.error(f"❌ Incorrect. The correct answer was {st.session_state.correct_answer}")
 
-    #Inline button generation and functionality
+    #Create inline buttons
     new_question, show_answer, return_to_menu = st.columns(3)
 
     if new_question.button("New Question", use_container_width=True):
@@ -123,7 +151,7 @@ else:
     
     if return_to_menu.button("Return to Menu", use_container_width= True):
         st.session_state.clear()
-        st.switch_page("pages/roulette_cash.py")
+        st.switch_page("pages/roulette_french_bets.py")
 
     # Autofocus text input
     st.components.v1.html(f"""
--- pages/roulette_menu.py
+++ pages/roulette_menu.py
@@ -7,7 +7,7 @@ payouts = middle.button("Payouts", use_container_width=True)
 cash_conversions = middle.button("Cash Conversions", use_container_width=True)
 straight_ups = middle.button("Straight Up Conversions", use_container_width=True)
 middle.divider()
-french_bets = middle.button("French Bets", use_container_width=True, disabled=True)
+french_bets = middle.button("French Bets", use_container_width=True)
 french_bets_cash = middle.button("French Bets - Cash", use_container_width=True, disabled=True)
 neighbour_bets = middle.button("Neighbour Bets", use_container_width=True, disabled=True)
 middle.divider()
@@ -19,5 +19,7 @@ if cash_conversions:
     st.switch_page("pages/roulette_cash.py")
 if straight_ups:
     st.switch_page("pages/roulette_straight_up.py")
+if french_bets:
+    st.switch_page("pages/roulette_french_bets.py")
 if return_to_menu:
     st.switch_page("main.py")
